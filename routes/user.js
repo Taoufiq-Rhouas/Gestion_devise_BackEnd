c2d96a2083d6bb48f3f76f0c9654049647cbdefb
Taoufiq-Rhouas <rhouastaoufiq@gmail.com>
2021-06-04 16:34:18 +0100

    forex_V6

--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 // const {checkUserIsExist, signup, getUserTotalAmount, checkUserTotalAmount, checkWalletTotalAmount, buyCrypto, sellCrypto, addBuyToWallet, addSellToWallet, getuserUnfo, getProfileInfo} = require('../controllers/userController')
 // const {addMoneyToWallet, signup} = require('../controllers/userController')
-const {signup,checkUserIsExistAndDeposit,getProfileInfo,getuserUnfo,getUserTotalAmount,checkUserTotalAmount,buyCrypto} = require('../controllers/userController');
+const {signup,checkUserIsExistAndDeposit,getProfileInfo,getuserUnfo,getUserTotalAmount,checkUserTotalAmount,buyCrypto,sellCrypto} = require('../controllers/userController');
 // const {getUserTotalAmount, checkUserTotalAmount, checkWalletTotalAmount, buyCrypto, sellCrypto, addBuyToWallet, addSellToWallet} = require('../controllers/userController')
 
 // const {SignUpValidator} = require('../middllwars/signInAndsignUpValidator')
@@ -11,6 +11,7 @@ const {signup,checkUserIsExistAndDeposit,getProfileInfo,getuserUnfo,getUserTotal
 router.post('/signUp', signup);
 router.put('/Deposit/',checkUserIsExistAndDeposit);
 router.post('/buyCrypto/',getUserTotalAmount, checkUserTotalAmount,buyCrypto);
+router.post('/sellCrypto/',getUserTotalAmount,sellCrypto);
 // router.post('/addMoneyToWallet', addMoneyToWallet)
 // router.post('/buy', getUserTotalAmount, checkUserTotalAmount, buyCrypto, addBuyToWallet)
 // router.post('/sell', getUserTotalAmount, checkWalletTotalAmount, sellCrypto, addSellToWallet)
