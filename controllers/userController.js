9e0e601fed431bd685f7e2e6e1e9140ff3402e1f
Taoufiq-Rhouas <rhouastaoufiq@gmail.com>
2021-06-04 11:37:56 +0100

    forex_V5

--- controllers/userController.js
+++ controllers/userController.js
@@ -87,57 +87,177 @@ exports.getuserUnfo = (req, res) => {
 
 
 exports.getUserTotalAmount = (req, res, next) => {
+    console.log("-- test 1 ----");
     User.findOne({email: req.body.email}, (error, Uemail) => {
+        console.log("-- test 2 ----");
         if(error)
             return res.status(400).json({error: error})
 
+        console.log("-- test 4 ----");
         req.userSold = Uemail;
         console.log(Uemail.sold)
+        console.log("-- test 5 ----");
+        // console.log(Uemail.sold)
             next()
     })
 }
 exports.checkUserTotalAmount = (req, res, next) => {
-    if(req.userSold.sold < req.body.SoldBuy)
+    console.log("-- test 6 ----");
+    if(req.userSold.sold < req.body.SoldDeposit)
         return res.status(400).json({error: "you not have enogth money to buy"})
 
     next()
 }
 
 exports.buyCrypto = (req, res, next) => {
+    console.log("-- test 7 ----");
     const conditions = {email: req.body.email}
     // const Crypto = req.body.Crypto;
     // console.log("Crypto : ",Crypto)
     // const update = {};
     if(req.body.Crypto == "MAD"){
-        update = {sold: req.userSold.sold - req.body.SoldBuy,SoldMAD: req.userSold.SoldMAD + req.body.SoldBuy}
+        // update = {sold: req.userSold.sold - req.body.SoldBuy,SoldMAD: req.userSold.SoldMAD + req.body.SoldBuy,SoldMADCryptobuy:req.userSold.SoldMADCryptobuy +  req.body.SoldCryptobuy}
+        update = {
+                    sold: req.userSold.sold - req.body.SoldDeposit,
+                    SoldMAD:{
+                        SoldMADdeposit: req.userSold.SoldMAD.SoldMADdeposit + req.body.SoldDeposit,
+                        SoldMADCryptobuy:req.userSold.SoldMAD.SoldMADCryptobuy +  req.body.SoldCryptobuy
+                    },
+                    // coins:{
+                    //     SoldMAD:{
+                    //         SoldMADdeposit: req.userSold.coins.SoldMAD.SoldMADdeposit + req.body.SoldDeposit,
+                    //         SoldMADCryptobuy:req.userSold.coins.SoldMAD.SoldMADCryptobuy +  req.body.SoldCryptobuy
+                    //     },
+                    // },
+                    // SoldMAD: req.userSold.SoldMAD + req.body.SoldBuy,
+                    // SoldMADCryptobuy:req.userSold.SoldMADCryptobuy +  req.body.SoldCryptobuy
+                }
     }else if(req.body.Crypto == "BTC"){
-        update = {sold: req.userSold.sold - req.body.SoldBuy,SoldBTC: req.userSold.SoldBTC + req.body.SoldBuy}
+        // update = {sold: req.userSold.sold - req.body.SoldBuy,SoldBTC: req.userSold.SoldBTC + req.body.SoldBuy}
+        update = {
+            sold: req.userSold.sold - req.body.SoldDeposit,
+            SoldBTC:{
+                SoldBTCdeposit: req.userSold.SoldBTC.SoldBTCdeposit + req.body.SoldDeposit,
+                SoldBTCCryptobuy:req.userSold.SoldBTC.SoldBTCCryptobuy +  req.body.SoldCryptobuy
+            },
+        }
     }else if(req.body.Crypto == "USD"){
-        update = {sold: req.userSold.sold - req.body.SoldBuy,SoldUSD: req.userSold.SoldUSD + req.body.SoldBuy}
+        // update = {sold: req.userSold.sold - req.body.SoldBuy,SoldUSD: req.userSold.SoldUSD + req.body.SoldBuy}
+        update = {
+            sold: req.userSold.sold - req.body.SoldDeposit,
+            SoldBTC:{
+                SoldBTCdeposit: req.userSold.SoldBTC.SoldBTCdeposit + req.body.SoldDeposit,
+                SoldBTCCryptobuy:req.userSold.SoldBTC.SoldBTCCryptobuy +  req.body.SoldCryptobuy
+            },
+        }
     }else if(req.body.Crypto == "BTC"){
-        update = {sold: req.userSold.sold - req.body.SoldBuy,SoldBTC: req.userSold.SoldBTC + req.body.SoldBuy}
+        // update = {sold: req.userSold.sold - req.body.SoldBuy,SoldBTC: req.userSold.SoldBTC + req.body.SoldBuy}
+        update = {
+            sold: req.userSold.sold - req.body.SoldDeposit,
+            SoldBTC:{
+                SoldBTCdeposit: req.userSold.SoldBTC.SoldBTCdeposit + req.body.SoldDeposit,
+                SoldBTCCryptobuy:req.userSold.SoldBTC.SoldBTCCryptobuy +  req.body.SoldCryptobuy
+            },
+        }
     }else if(req.body.Crypto == "GBP"){
-        update = {sold: req.userSold.sold - req.body.SoldBuy,SoldGBP: req.userSold.SoldGBP + req.body.SoldBuy}
+        // update = {sold: req.userSold.sold - req.body.SoldBuy,SoldGBP: req.userSold.SoldGBP + req.body.SoldBuy}
+        update = {
+            sold: req.userSold.sold - req.body.SoldDeposit,
+            SoldGBP:{
+                SoldGBPdeposit: req.userSold.SoldGBP.SoldGBPdeposit + req.body.SoldDeposit,
+                SoldGBPCryptobuy:req.userSold.SoldGBP.SoldGBPCryptobuy +  req.body.SoldCryptobuy
+            },
+        }
     }else if(req.body.Crypto == "KWD"){
-        update = {sold: req.userSold.sold - req.body.SoldBuy,SoldKWD: req.userSold.SoldKWD + req.body.SoldBuy}
+        // update = {sold: req.userSold.sold - req.body.SoldBuy,SoldKWD: req.userSold.SoldKWD + req.body.SoldBuy}
+        update = {
+            sold: req.userSold.sold - req.body.SoldDeposit,
+            SoldKWD:{
+                SoldKWDdeposit: req.userSold.SoldKWD.SoldKWDdeposit + req.body.SoldDeposit,
+                SoldKWDCryptobuy:req.userSold.SoldKWD.SoldKWDCryptobuy +  req.body.SoldCryptobuy
+            },
+        }
     }else if(req.body.Crypto == "AED"){
-        update = {sold: req.userSold.sold - req.body.SoldBuy,SoldAED: req.userSold.SoldAED + req.body.SoldBuy}
+        // update = {sold: req.userSold.sold - req.body.SoldBuy,SoldAED: req.userSold.SoldAED + req.body.SoldBuy}
+        update = {
+            sold: req.userSold.sold - req.body.SoldDeposit,
+            SoldAED:{
+                SoldAEDdeposit: req.userSold.SoldAED.SoldAEDdeposit + req.body.SoldDeposit,
+                SoldAEDCryptobuy:req.userSold.SoldAED.SoldAEDCryptobuy +  req.body.SoldCryptobuy
+            },
+        }
     }else if(req.body.Crypto == "AFN"){
-        update = {sold: req.userSold.sold - req.body.SoldBuy,SoldAFN: req.userSold.SoldAFN + req.body.SoldBuy}
+        // update = {sold: req.userSold.sold - req.body.SoldBuy,SoldAFN: req.userSold.SoldAFN + req.body.SoldBuy}
+        update = {
+            sold: req.userSold.sold - req.body.SoldDeposit,
+            SoldAFN:{
+                SoldAFNdeposit: req.userSold.SoldAFN.SoldAFNdeposit + req.body.SoldDeposit,
+                SoldAFNCryptobuy:req.userSold.SoldAFN.SoldAFNCryptobuy +  req.body.SoldCryptobuy
+            },
+        }
     }else if(req.body.Crypto == "ALL"){
-        update = {sold: req.userSold.sold - req.body.SoldBuy,SoldALL: req.userSold.SoldALL + req.body.SoldBuy}
+        // update = {sold: req.userSold.sold - req.body.SoldBuy,SoldALL: req.userSold.SoldALL + req.body.SoldBuy}
+        update = {
+            sold: req.userSold.sold - req.body.SoldDeposit,
+            SoldALL:{
+                SoldALLdeposit: req.userSold.SoldALL.SoldALLdeposit + req.body.SoldDeposit,
+                SoldALLCryptobuy:req.userSold.SoldALL.SoldALLCryptobuy +  req.body.SoldCryptobuy
+            },
+        }
     }else if(req.body.Crypto == "AMD"){
-        update = {sold: req.userSold.sold - req.body.SoldBuy,SoldAMD: req.userSold.SoldAMD + req.body.SoldBuy}
+        // update = {sold: req.userSold.sold - req.body.SoldBuy,SoldAMD: req.userSold.SoldAMD + req.body.SoldBuy}
+        update = {
+            sold: req.userSold.sold - req.body.SoldDeposit,
+            SoldAMD:{
+                SoldAMDdeposit: req.userSold.SoldAMD.SoldAMDdeposit + req.body.SoldDeposit,
+                SoldAMDCryptobuy:req.userSold.SoldAMD.SoldAMDCryptobuy +  req.body.SoldCryptobuy
+            },
+        }
     }else if(req.body.Crypto == "ANG"){
-        update = {sold: req.userSold.sold - req.body.SoldBuy,SoldANG: req.userSold.SoldANG + req.body.SoldBuy}
+        // update = {sold: req.userSold.sold - req.body.SoldBuy,SoldANG: req.userSold.SoldANG + req.body.SoldBuy}
+        update = {
+            sold: req.userSold.sold - req.body.SoldDeposit,
+            SoldANG:{
+                SoldANGdeposit: req.userSold.SoldANG.SoldANGdeposit + req.body.SoldDeposit,
+                SoldANGCryptobuy:req.userSold.SoldANG.SoldANGCryptobuy +  req.body.SoldCryptobuy
+            },
+        }
     }else if(req.body.Crypto == "AOA"){
-        update = {sold: req.userSold.sold - req.body.SoldBuy,SoldAOA: req.userSold.SoldAOA + req.body.SoldBuy}
+        // update = {sold: req.userSold.sold - req.body.SoldBuy,SoldAOA: req.userSold.SoldAOA + req.body.SoldBuy}
+        update = {
+            sold: req.userSold.sold - req.body.SoldDeposit,
+            SoldAOA:{
+                SoldAOAdeposit: req.userSold.SoldAOA.SoldAOAdeposit + req.body.SoldDeposit,
+                SoldAOACryptobuy:req.userSold.SoldAOA.SoldAOACryptobuy +  req.body.SoldCryptobuy
+            },
+        }
     }else if(req.body.Crypto == "ARS"){
-        update = {sold: req.userSold.sold - req.body.SoldBuy,SoldARS: req.userSold.SoldARS + req.body.SoldBuy}
+        // update = {sold: req.userSold.sold - req.body.SoldBuy,SoldARS: req.userSold.SoldARS + req.body.SoldBuy}
+        update = {
+            sold: req.userSold.sold - req.body.SoldDeposit,
+            SoldARS:{
+                SoldARSdeposit: req.userSold.SoldARS.SoldARSdeposit + req.body.SoldDeposit,
+                SoldARSCryptobuy:req.userSold.SoldARS.SoldARSCryptobuy +  req.body.SoldCryptobuy
+            },
+        }
     }else if(req.body.Crypto == "AUD"){
-        update = {sold: req.userSold.sold - req.body.SoldBuy,SoldAUD: req.userSold.SoldAUD + req.body.SoldBuy}
+        // update = {sold: req.userSold.sold - req.body.SoldBuy,SoldAUD: req.userSold.SoldAUD + req.body.SoldBuy}
+        update = {
+            sold: req.userSold.sold - req.body.SoldDeposit,
+            SoldAUD:{
+                SoldAUDdeposit: req.userSold.SoldAUD.SoldAUDdeposit + req.body.SoldDeposit,
+                SoldAUDCryptobuy:req.userSold.SoldAUD.SoldAUDCryptobuy +  req.body.SoldCryptobuy
+            },
+        }
     }else if(req.body.Crypto == "AWG"){
-        update = {sold: req.userSold.sold - req.body.SoldBuy,SoldAWG: req.userSold.SoldAWG + req.body.SoldBuy}
+        // update = {sold: req.userSold.sold - req.body.SoldBuy,SoldAWG: req.userSold.SoldAWG + req.body.SoldBuy}
+        update = {
+            sold: req.userSold.sold - req.body.SoldDeposit,
+            SoldAWG:{
+                SoldAWGdeposit: req.userSold.SoldAWG.SoldAWGdeposit + req.body.SoldDeposit,
+                SoldAWGCryptobuy:req.userSold.SoldAWG.SoldAWGCryptobuy +  req.body.SoldCryptobuy
+            },
+        }
     }
     // const options = {upsert: true}
 
--- models/User.js
+++ models/User.js
@@ -18,66 +18,405 @@ const userSchema = new mongoose.Schema({
     //     type: String,
     //     required: true
     // },
+    // SoldMAD: {
+    //     type: Number,
+    //     require: true,
+    //     default: 0
+    // },
+    //  V Coins /
+    // coins:{
+    //     SoldMAD: {
+    //         SoldMADdeposit: {
+    //             type: Number,
+    //             require: true,
+    //             default: 0
+    //         },
+    //         SoldMADCryptobuy: {
+    //             type: Number,
+    //             require: true,
+    //             default: 0
+    //         }
+    //     },
+    //     SoldBTC: {
+    //         SoldBTCdeposit: {
+    //             type: Number,
+    //             require: true,
+    //             default: 0
+    //         },
+    //         SoldBTCCryptobuy: {
+    //             type: Number,
+    //             require: true,
+    //             default: 0
+    //         }
+    //     },
+    //     SoldUSD: {
+    //         SoldUSDdeposit: {
+    //             type: Number,
+    //             require: true,
+    //             default: 0
+    //         },
+    //         SoldUSDCryptobuy: {
+    //             type: Number,
+    //             require: true,
+    //             default: 0
+    //         }
+    //     },
+    //     SoldGBP: {
+    //         SoldGBPdeposit: {
+    //             type: Number,
+    //             require: true,
+    //             default: 0
+    //         },
+    //         SoldGBPCryptobuy: {
+    //             type: Number,
+    //             require: true,
+    //             default: 0
+    //         }
+    //     },
+    //     SoldKWD: {
+    //         SoldKWDdeposit: {
+    //             type: Number,
+    //             require: true,
+    //             default: 0
+    //         },
+    //         SoldKWDCryptobuy: {
+    //             type: Number,
+    //             require: true,
+    //             default: 0
+    //         }
+    //     },
+    //     SoldAED: {
+    //         SoldAEDdeposit: {
+    //             type: Number,
+    //             require: true,
+    //             default: 0
+    //         },
+    //         SoldAEDCryptobuy: {
+    //             type: Number,
+    //             require: true,
+    //             default: 0
+    //         }
+    //     },
+    //     SoldAFN: {
+    //         SoldAFNdeposit: {
+    //             type: Number,
+    //             require: true,
+    //             default: 0
+    //         },
+    //         SoldAFNCryptobuy: {
+    //             type: Number,
+    //             require: true,
+    //             default: 0
+    //         }
+    //     },
+    //     SoldALL: {
+    //         SoldALLdeposit: {
+    //             type: Number,
+    //             require: true,
+    //             default: 0
+    //         },
+    //         SoldALLCryptobuy: {
+    //             type: Number,
+    //             require: true,
+    //             default: 0
+    //         }
+    //     },
+    //     SoldAMD: {
+    //         SoldAMDdeposit: {
+    //             type: Number,
+    //             require: true,
+    //             default: 0
+    //         },
+    //         SoldAMDCryptobuy: {
+    //             type: Number,
+    //             require: true,
+    //             default: 0
+    //         }
+    //     },
+    //     SoldANG: {
+    //         SoldANGdeposit: {
+    //             type: Number,
+    //             require: true,
+    //             default: 0
+    //         },
+    //         SoldANGCryptobuy: {
+    //             type: Number,
+    //             require: true,
+    //             default: 0
+    //         }
+    //     },
+    //     SoldAOA: {
+    //         SoldAOAdeposit: {
+    //             type: Number,
+    //             require: true,
+    //             default: 0
+    //         },
+    //         SoldAOACryptobuy: {
+    //             type: Number,
+    //             require: true,
+    //             default: 0
+    //         }
+    //     },
+    //     SoldARS: {
+    //         SoldARSdeposit: {
+    //             type: Number,
+    //             require: true,
+    //             default: 0
+    //         },
+    //         SoldARSCryptobuy: {
+    //             type: Number,
+    //             require: true,
+    //             default: 0
+    //         }
+    //     },
+    //     SoldAUD: {
+    //         SoldAUDdeposit: {
+    //             type: Number,
+    //             require: true,
+    //             default: 0
+    //         },
+    //         SoldAUDCryptobuy: {
+    //             type: Number,
+    //             require: true,
+    //             default: 0
+    //         }
+    //     },
+    //     SoldAWG: {
+    //         SoldAWGdeposit: {
+    //             type: Number,
+    //             require: true,
+    //             default: 0
+    //         },
+    //         SoldAWGCryptobuy: {
+    //             type: Number,
+    //             require: true,
+    //             default: 0
+    //         }
+    //     },
+    // },
+    // END V Coins
     SoldMAD: {
-        type: Number,
-        require: true,
-        default: 0
+        SoldMADdeposit: {
+            type: Number,
+            require: true,
+            default: 0
+        },
+        SoldMADCryptobuy: {
+            type: Number,
+            require: true,
+            default: 0
+        }
     },
     SoldBTC: {
-        type: Number,
-        require: true,
-        default: 0
-    },
-    SoldUSD: {
-        type: Number,
-        require: true,
-        default: 0
+        SoldBTCdeposit: {
+            type: Number,
+            require: true,
+            default: 0
+        },
+        SoldBTCCryptobuy: {
+            type: Number,
+            require: true,
+            default: 0
+        }
     },
     SoldGBP: {
-        type: Number,
-        require: true,
-        default: 0
+        SoldGBPdeposit: {
+            type: Number,
+            require: true,
+            default: 0
+        },
+        SoldGBPCryptobuy: {
+            type: Number,
+            require: true,
+            default: 0
+        }
     },
     SoldKWD: {
-        type: Number,
-        require: true,
-        default: 0
+        SoldKWDdeposit: {
+            type: Number,
+            require: true,
+            default: 0
+        },
+        SoldKWDCryptobuy: {
+            type: Number,
+            require: true,
+            default: 0
+        }
     },
     SoldAED: {
-        type: Number,
-        require: true,
-        default: 0
+        SoldAEDdeposit: {
+            type: Number,
+            require: true,
+            default: 0
+        },
+        SoldAEDCryptobuy: {
+            type: Number,
+            require: true,
+            default: 0
+        }
     },
     SoldAFN: {
-        type: Number,
-        require: true,
-        default: 0
+        SoldAFNdeposit: {
+            type: Number,
+            require: true,
+            default: 0
+        },
+        SoldAFNCryptobuy: {
+            type: Number,
+            require: true,
+            default: 0
+        }
     },
     SoldALL: {
-        type: Number,
-        require: true,
-        default: 0
+        SoldALLdeposit: {
+            type: Number,
+            require: true,
+            default: 0
+        },
+        SoldALLCryptobuy: {
+            type: Number,
+            require: true,
+            default: 0
+        }
     },
     SoldAMD: {
-        type: Number,
-        require: true,
-        default: 0
+        SoldAMDdeposit: {
+            type: Number,
+            require: true,
+            default: 0
+        },
+        SoldAMDCryptobuy: {
+            type: Number,
+            require: true,
+            default: 0
+        }
     },
     SoldANG: {
-        type: Number,
-        require: true,
-        default: 0
+        SoldANGdeposit: {
+            type: Number,
+            require: true,
+            default: 0
+        },
+        SoldANGCryptobuy: {
+            type: Number,
+            require: true,
+            default: 0
+        }
     },
     SoldAOA: {
-        type: Number,
-        require: true,
-        default: 0
+        SoldAOAdeposit: {
+            type: Number,
+            require: true,
+            default: 0
+        },
+        SoldAOACryptobuy: {
+            type: Number,
+            require: true,
+            default: 0
+        }
     },
     SoldARS: {
-        type: Number,
-        require: true,
-        default: 0
+        SoldARSdeposit: {
+            type: Number,
+            require: true,
+            default: 0
+        },
+        SoldARSCryptobuy: {
+            type: Number,
+            require: true,
+            default: 0
+        }
     },
+    SoldAUD: {
+        SoldAUDdeposit: {
+            type: Number,
+            require: true,
+            default: 0
+        },
+        SoldAUDCryptobuy: {
+            type: Number,
+            require: true,
+            default: 0
+        }
+    },
+    SoldAWG: {
+        SoldAWGdeposit: {
+            type: Number,
+            require: true,
+            default: 0
+        },
+        SoldAWGCryptobuy: {
+            type: Number,
+            require: true,
+            default: 0
+        }
+    },
+
+    // SoldMAD: {
+    //     type: Number,
+    //     require: true,
+    //     default: 0
+    // },
+    // SoldMADCryptobuy: {
+    //     type: Number,
+    //     require: true,
+    //     default: 0
+    // },
+    // SoldBTC: {
+    //     type: Number,
+    //     require: true,
+    //     default: 0
+    // },
+    // SoldUSD: {
+    //     type: Number,
+    //     require: true,
+    //     default: 0
+    // },
+    // SoldGBP: {
+    //     type: Number,
+    //     require: true,
+    //     default: 0
+    // },
+    // SoldKWD: {
+    //     type: Number,
+    //     require: true,
+    //     default: 0
+    // },
+    // SoldAED: {
+    //     type: Number,
+    //     require: true,
+    //     default: 0
+    // },
+    // SoldAFN: {
+    //     type: Number,
+    //     require: true,
+    //     default: 0
+    // },
+    // SoldALL: {
+    //     type: Number,
+    //     require: true,
+    //     default: 0
+    // },
+    // SoldAMD: {
+    //     type: Number,
+    //     require: true,
+    //     default: 0
+    // },
+    // SoldANG: {
+    //     type: Number,
+    //     require: true,
+    //     default: 0
+    // },
+    // SoldAOA: {
+    //     type: Number,
+    //     require: true,
+    //     default: 0
+    // },
+    // SoldARS: {
+    //     type: Number,
+    //     require: true,
+    //     default: 0
+    // },
     sold: {
         type: Number,
         require: true,
